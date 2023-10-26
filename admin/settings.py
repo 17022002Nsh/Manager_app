dc86a9058bb0a557784637390042bba56c88f5d4
Shaxboz <narzullayev2@gmail.com>
2023-10-26 18:17:17 +0500

    p

--- admin/settings.py
+++ admin/settings.py
@@ -197,7 +197,7 @@ SIMPLE_JWT = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'narzullayevshaxboz2@gmail.com'
-EMAIL_HOST_PASSWORD = 'rzho chai depn vxcl' #past the key or password app here
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD') #past the key or password app here
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
