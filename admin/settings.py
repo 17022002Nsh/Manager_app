ce605e1a9e4715a6a8198f0ff22ec1a3b2ed99ee
Shaxboz <narzullayev2@gmail.com>
2023-10-26 15:06:44 +0500

    M

--- admin/settings.py
+++ admin/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
 from datetime import timedelta
+import os 
+from dotenv import load_dotenv
+load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b@ar%bt*xmgcxax+9aj7dp2ce7p+8aqsh@o&72zu(tq0hgd458'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv("DEBUG") == "True")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*'] if DEBUG else os.getenv("ALLOWED_HOSTS").split(",")
 
 
 # Application definition
